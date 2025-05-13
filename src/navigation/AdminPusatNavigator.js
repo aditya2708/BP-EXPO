3736248b7f4051a54c38dedb5eafaeb1d8a04d39
aditya2708 <adityahadywijaya@gmail.com>
2025-05-13 19:19:59 +0700

    vvvv

--- src/navigation/AdminPusatNavigator.js
+++ src/navigation/AdminPusatNavigator.js
@@ -12,6 +12,23 @@ import AdminPusatProfileScreen from '../features/adminPusat/screens/AdminPusatPr
 // import KacabDetailScreen from '../features/adminPusat/screens/KacabDetailScreen';
 // import SettingsScreen from '../features/adminPusat/screens/SettingsScreen';
 
+//Anak Screen
+import AnakManagementScreen from '../features/adminPusat/screens/AnakManagementScreen';
+import AnakDetailScreen from '../features/adminPusat/screens/AnakDetailScreen';
+//import AnakFormScreen from '../features/adminPusat/screens/AnakFormScreen';
+// Import Anak Detail Screens
+import InformasiAnakScreen from '../features/adminPusat/screens/anakDetail/InformasiAnakScreen';
+import RaportScreen from '../features/adminPusat/screens/anakDetail/RaportScreen';
+//import RaportDetailScreen from '../features/adminPusat/screens/anakDetail/RaportDetailScreen';
+import PrestasiScreen from '../features/adminPusat/screens/anakDetail/PrestasiScreen';
+//import PrestasiDetailScreen from '../features/adminPusat/screens/anakDetail/PrestasiDetailScreen';
+import RiwayatScreen from '../features/adminPusat/screens/anakDetail/RiwayatScreen';
+//import RiwayatDetailScreen from '../features/adminPusat/screens/anakDetail/RiwayatDetailScreen';
+//import SuratScreen from '../features/adminPusat/screens/anakDetail/SuratScreen';
+//import CeritaScreen from '../features/adminPusat/screens/anakDetail/CeritaScreen';
+//import NilaiAnakScreen from '../features/adminPusat/screens/anakDetail/NilaiAnakScreen';
+//import RaporShelterScreen from '../features/adminPusat/screens/anakDetail/RaporShelterScreen';
+
 // Create navigators
 const Tab = createBottomTabNavigator();
 const HomeStack = createStackNavigator();
@@ -59,6 +76,82 @@ const ManagementStackNavigator = () => {
         options={({ route }) => ({ 
           headerTitle: route.params?.title || 'Cabang Detail' 
         })}
+      /> */}
+         <ManagementStack.Screen 
+        name="AnakManagement" 
+        component={AnakManagementScreen} 
+        options={{ headerTitle: 'Children Management' }}
+      />
+      <ManagementStack.Screen 
+        name="AnakDetail" 
+        component={AnakDetailScreen} 
+        options={({ route }) => ({ 
+          headerTitle: route.params?.isNew ? 'Add New Child' : (route.params?.title || 'Child Detail')
+        })}
+      />
+      {/* <ManagementStack.Screen 
+        name="AnakForm" 
+        component={AnakFormScreen} 
+        options={({ route }) => ({ 
+          headerTitle: route.params?.isEdit ? 'Edit Child' : 'Add New Child'
+        })}
+      /> */}
+      
+      {/* Anak Detail Screens */}
+      <ManagementStack.Screen 
+        name="InformasiAnak" 
+        component={InformasiAnakScreen} 
+        options={{ headerTitle: 'Informasi Anak' }}
+      />
+      <ManagementStack.Screen 
+        name="Raport" 
+        component={RaportScreen} 
+        options={{ headerTitle: 'Raport Anak' }}
+      />
+      {/* <ManagementStack.Screen 
+        name="RaportDetail" 
+        component={RaportDetailScreen} 
+        options={{ headerTitle: 'Detail Raport' }}
+      /> */}
+      <ManagementStack.Screen 
+        name="Prestasi" 
+        component={PrestasiScreen} 
+        options={{ headerTitle: 'Prestasi Anak' }}
+      />
+      {/* <ManagementStack.Screen 
+        name="PrestasiDetail" 
+        component={PrestasiDetailScreen} 
+        options={{ headerTitle: 'Detail Prestasi' }}
+      /> */}
+      <ManagementStack.Screen 
+        name="Riwayat" 
+        component={RiwayatScreen} 
+        options={{ headerTitle: 'Riwayat Anak' }}
+      />
+      {/* <ManagementStack.Screen 
+        name="RiwayatDetail" 
+        component={RiwayatDetailScreen} 
+        options={{ headerTitle: 'Detail Riwayat' }}
+      /> */}
+      {/* <ManagementStack.Screen 
+        name="Surat" 
+        component={SuratScreen} 
+        options={{ headerTitle: 'Surat Anak' }}
+      />
+      <ManagementStack.Screen 
+        name="Cerita" 
+        component={CeritaScreen} 
+        options={{ headerTitle: 'Cerita Anak' }}
+      /> */}
+      {/* <ManagementStack.Screen 
+        name="NilaiAnak" 
+        component={NilaiAnakScreen} 
+        options={{ headerTitle: 'Nilai Anak' }}
+      /> */}
+      {/* <ManagementStack.Screen 
+        name="RaporShelter" 
+        component={RaporShelterScreen} 
+        options={{ headerTitle: 'Rapor Shelter' }}
       /> */}
     </ManagementStack.Navigator>
   );
