4315321e597214ca6655e0ab2bc2475255cbdff7
aditya2708 <adityahadywijaya@gmail.com>
2025-06-12 15:06:08 +0700

    kamis

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,6 +1,6 @@
 // API base URLs
-//export const API_BASE_URL = 'http://192.168.1.13:8000/api';
-export const API_BASE_URL = 'http://192.168.8.105:8000/api';
+export const API_BASE_URL = 'http://192.168.1.13:8000/api';
+//export const API_BASE_URL = 'http://192.168.8.105:8000/api';
 
 // Storage keys
 export const STORAGE_TOKEN_KEY = 'berbagipendidikan_token';
