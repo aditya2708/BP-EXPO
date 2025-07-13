a8aef26675779e4a07080056d89d7ab889a8235b
aditya2708 <adityahadywijaya@gmail.com>
2025-07-13 20:47:36 +0700

    Add files via upload

--- src/navigation/AdminCabangNavigator.js
+++ src/navigation/AdminCabangNavigator.js
@@ -12,27 +12,21 @@ import AdminCabangProfileScreen from '../features/adminCabang/screen/AdminCabang
 import MasterDataMenuScreen from '../features/adminCabang/screen/MasterDataMenuScreen';
 import AkademikMenuScreen from '../features/adminCabang/screen/AkademikMenuScreen';
 
-// Master Data - Jenjang Screens
+// Master Data Screens
 import JenjangListScreen from '../features/adminCabang/screen/masterData/jenjang/JenjangListScreen';
 import JenjangFormScreen from '../features/adminCabang/screen/masterData/jenjang/JenjangFormScreen';
 import JenjangDetailScreen from '../features/adminCabang/screen/masterData/jenjang/JenjangDetailScreen';
-
-// Master Data - Mata Pelajaran Screens
 import MataPelajaranListScreen from '../features/adminCabang/screen/masterData/mataPelajaran/MataPelajaranListScreen';
 import MataPelajaranFormScreen from '../features/adminCabang/screen/masterData/mataPelajaran/MataPelajaranFormScreen';
 import MataPelajaranDetailScreen from '../features/adminCabang/screen/masterData/mataPelajaran/MataPelajaranDetailScreen';
-
-// Master Data - Kelas Screens
 import KelasListScreen from '../features/adminCabang/screen/masterData/kelas/KelasListScreen';
 import KelasFormScreen from '../features/adminCabang/screen/masterData/kelas/KelasFormScreen';
 import KelasDetailScreen from '../features/adminCabang/screen/masterData/kelas/KelasDetailScreen';
-
-// Master Data - Materi Screens
 import MateriListScreen from '../features/adminCabang/screen/masterData/materi/MateriListScreen';
 import MateriFormScreen from '../features/adminCabang/screen/masterData/materi/MateriFormScreen';
 import MateriDetailScreen from '../features/adminCabang/screen/masterData/materi/MateriDetailScreen';
 
-// Akademik - Kurikulum Screens
+// Akademik Screens
 import KurikulumListScreen from '../features/adminCabang/screen/akademik/kurikulum/KurikulumListScreen';
 import KurikulumFormScreen from '../features/adminCabang/screen/akademik/kurikulum/KurikulumFormScreen';
 import KurikulumDetailScreen from '../features/adminCabang/screen/akademik/kurikulum/KurikulumDetailScreen';
@@ -43,11 +37,6 @@ import AdminCabangDonaturManagementScreen from '../features/adminCabang/screen/A
 import AdminCabangDonaturFormScreen from '../features/adminCabang/screen/AdminCabangDonaturFormScreen';
 import AdminCabangDonaturDetailScreen from '../features/adminCabang/screen/AdminCabangDonaturDetailScreen';
 import AdminCabangPengajuanDonaturScreen from '../features/adminCabang/screen/AdminCabangPengajuanDonaturScreen';
-// import AdminCabangPengajuanDonaturDetailScreen from '../features/adminCabang/screen/AdminCabangPengajuanDonaturDetailScreen';
-
-// Survey Management Screens
-// import AdminCabangSurveyManagementScreen from '../features/adminCabang/screen/AdminCabangSurveyManagementScreen';
-// import AdminCabangSurveyDetailScreen from '../features/adminCabang/screen/AdminCabangSurveyDetailScreen';
 
 const Tab = createBottomTabNavigator();
 const HomeStack = createStackNavigator();
@@ -63,53 +52,40 @@ const defaultScreenOptions = {
   headerTitleStyle: { fontWeight: 'bold' },
 };
 
-// Home Stack Navigator
+// Stack Navigators
 const HomeStackNavigator = () => (
   <HomeStack.Navigator screenOptions={{ headerShown: false }}>
     <HomeStack.Screen name="Dashboard" component={AdminCabangDashboardScreen} />
   </HomeStack.Navigator>
 );
 
-// Master Data Stack Navigator
 const MasterDataStackNavigator = () => (
   <MasterDataStack.Navigator screenOptions={{ headerShown: false }}>
     <MasterDataStack.Screen name="MasterDataMenu" component={MasterDataMenuScreen} />
-    
-    {/* Jenjang Screens */}
     <MasterDataStack.Screen name="JenjangList" component={JenjangListScreen} />
     <MasterDataStack.Screen name="JenjangForm" component={JenjangFormScreen} />
     <MasterDataStack.Screen name="JenjangDetail" component={JenjangDetailScreen} />
-    
-    {/* Mata Pelajaran Screens */}
     <MasterDataStack.Screen name="MataPelajaranList" component={MataPelajaranListScreen} />
     <MasterDataStack.Screen name="MataPelajaranForm" component={MataPelajaranFormScreen} />
     <MasterDataStack.Screen name="MataPelajaranDetail" component={MataPelajaranDetailScreen} />
-    
-    {/* Kelas Screens */}
     <MasterDataStack.Screen name="KelasList" component={KelasListScreen} />
     <MasterDataStack.Screen name="KelasForm" component={KelasFormScreen} />
     <MasterDataStack.Screen name="KelasDetail" component={KelasDetailScreen} />
-    
-    {/* Materi Screens */}
     <MasterDataStack.Screen name="MateriList" component={MateriListScreen} />
     <MasterDataStack.Screen name="MateriForm" component={MateriFormScreen} />
     <MasterDataStack.Screen name="MateriDetail" component={MateriDetailScreen} />
   </MasterDataStack.Navigator>
 );
 
-// Akademik Stack Navigator
 const AkademikStackNavigator = () => (
   <AkademikStack.Navigator screenOptions={{ headerShown: false }}>
     <AkademikStack.Screen name="AkademikMenu" component={AkademikMenuScreen} />
-    
-    {/* Kurikulum Screens */}
     <AkademikStack.Screen name="KurikulumList" component={KurikulumListScreen} />
     <AkademikStack.Screen name="KurikulumForm" component={KurikulumFormScreen} />
     <AkademikStack.Screen name="KurikulumDetail" component={KurikulumDetailScreen} />
   </AkademikStack.Navigator>
 );
 
-// Management Stack Navigator
 const ManagementStackNavigator = () => (
   <ManagementStack.Navigator screenOptions={defaultScreenOptions}>
     <ManagementStack.Screen 
@@ -117,20 +93,6 @@ const ManagementStackNavigator = () => (
       component={AdminCabangManagementMenuScreen} 
       options={{ title: 'Manajemen' }}
     />
-    
-    {/* Survey Management */}
-    <ManagementStack.Screen 
-      name="SurveyManagement" 
-      component={AdminCabangSurveyManagementScreen} 
-      options={{ title: 'Survey Management' }}
-    />
-    <ManagementStack.Screen 
-      name="SurveyDetail" 
-      component={AdminCabangSurveyDetailScreen} 
-      options={{ title: 'Detail Survey' }}
-    />
-    
-    {/* Donatur Management */}
     <ManagementStack.Screen 
       name="DonaturManagement" 
       component={AdminCabangDonaturManagementScreen} 
@@ -149,7 +111,6 @@ const ManagementStackNavigator = () => (
   </ManagementStack.Navigator>
 );
 
-// Pengajuan Donatur Stack Navigator
 const PengajuanDonaturStackNavigator = () => (
   <PengajuanDonaturStack.Navigator screenOptions={defaultScreenOptions}>
     <PengajuanDonaturStack.Screen 
@@ -157,15 +118,9 @@ const PengajuanDonaturStackNavigator = () => (
       component={AdminCabangPengajuanDonaturScreen} 
       options={{ title: 'Pengajuan Donatur' }}
     />
-    {/* <PengajuanDonaturStack.Screen 
-      name="PengajuanDonaturDetail" 
-      component={AdminCabangPengajuanDonaturDetailScreen} 
-      options={{ title: 'Detail Pengajuan' }}
-    /> */}
   </PengajuanDonaturStack.Navigator>
 );
 
-// Profile Stack Navigator
 const ProfileStackNavigator = () => (
   <ProfileStack.Navigator screenOptions={defaultScreenOptions}>
     <ProfileStack.Screen 
@@ -195,37 +150,38 @@ export default function AdminCabangNavigator() {
         tabBarActiveTintColor: '#2ecc71',
         tabBarInactiveTintColor: 'gray',
         tabBarStyle: { paddingBottom: 5, height: 60 },
+        tabBarLabelStyle: { fontSize: 12 },
         headerShown: false,
       })}
     >
       <Tab.Screen 
         name="Home" 
-        component={HomeStackNavigator}
+        component={HomeStackNavigator} 
         options={{ tabBarLabel: 'Beranda' }}
       />
       <Tab.Screen 
         name="MasterData" 
-        component={MasterDataStackNavigator}
-        options={{ tabBarLabel: 'Master Data' }}
+        component={MasterDataStackNavigator} 
+        options={{ tabBarLabel: 'Master D..' }}
       />
       <Tab.Screen 
         name="Akademik" 
-        component={AkademikStackNavigator}
+        component={AkademikStackNavigator} 
         options={{ tabBarLabel: 'Akademik' }}
       />
       <Tab.Screen 
         name="Management" 
-        component={ManagementStackNavigator}
-        options={{ tabBarLabel: 'Manajemen' }}
+        component={ManagementStackNavigator} 
+        options={{ tabBarLabel: 'Manajem..' }}
       />
       <Tab.Screen 
         name="PengajuanDonatur" 
-        component={PengajuanDonaturStackNavigator}
+        component={PengajuanDonaturStackNavigator} 
         options={{ tabBarLabel: 'Pengajuan' }}
       />
       <Tab.Screen 
         name="Profile" 
-        component={ProfileStackNavigator}
+        component={ProfileStackNavigator} 
         options={{ tabBarLabel: 'Profil' }}
       />
     </Tab.Navigator>
