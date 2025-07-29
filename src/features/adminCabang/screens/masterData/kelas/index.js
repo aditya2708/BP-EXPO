21739bc1275984739cca2261ef3d9f454d062b0c
aditya2708 <adityahadywijaya@gmail.com>
2025-07-29 14:03:04 +0700

    Delete src/features/adminCabang directory

--- src/features/adminCabang/screens/masterData/kelas/index.js
+++
@@ -1,5 +0,0 @@
-// Export all kelas screens for easy importing
-export { default as KelasListScreen } from './KelasListScreen';
-export { default as KelasFormScreen } from './KelasFormScreen';
-export { default as KelasDetailScreen } from './KelasDetailScreen';
-export { default as KelasStatsScreen } from './KelasStatsScreen';
