5bfe1e1a77d627e22a6c548b75fae1f2fe97d4ea
aditya2708 <adityahadywijaya@gmail.com>
2025-07-30 07:00:55 +0700

    Delete src/features/adminCabang directory

--- src/features/adminCabang/api/masterData/index.js
+++
@@ -1,5 +0,0 @@
-// Export all master data APIs for easy importing
-export { jenjangApi } from './jenjangApi';
-export { mataPelajaranApi } from './mataPelajaranApi';
-export { kelasApi } from './kelasApi';
-export { materiApi } from './materiApi';
--- src/features/adminCabang/components/base/index.js
+++
@@ -1,3 +0,0 @@
-// Export all base components for easy importing
-export { default as BaseListScreen } from './BaseListScreen';
-export { default as BaseFormScreen } from './BaseFormScreen';
