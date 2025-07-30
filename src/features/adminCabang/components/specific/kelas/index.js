21d1c0a49037f8ed3868050520b5479f6f6f5f73
aditya2708 <adityahadywijaya@gmail.com>
2025-07-30 19:32:22 +0700

    Delete src/features/adminCabang directory

--- src/features/adminCabang/components/specific/kelas/index.js
+++
@@ -1,7 +0,0 @@
-// Export all kelas components for easy importing
-export { default as KelasCard } from './KelasCard';
-export { default as KelasForm } from './KelasForm';
-export { default as JenisKelasToggle } from './JenisKelasToggle';
-export { default as TingkatSelector } from './TingkatSelector';
-export { default as KelasValidationDialog } from './KelasValidationDialog';
-export { default as KelasFilter } from './KelasFilter';
