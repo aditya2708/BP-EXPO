9925b435f30d158ec9cf7339a7b756acf397f9ec
aditya2708 <adityahadywijaya@gmail.com>
2025-07-30 19:21:36 +0700

    Delete src/features/adminCabang directory

--- src/features/adminCabang/components/specific/materi/index.js
+++
@@ -1,5 +0,0 @@
-// Export all materi components for easy importing
-export { default as MateriCard } from './MateriCard';
-export { default as MateriForm } from './MateriForm';
-export { default as MateriCascadeFilter } from './MateriCascadeFilter';
-export { default as MateriUsageIndicator } from './MateriUsageIndicator';
--- src/features/adminCabang/screens/masterData/materi/index.js
+++
@@ -1,5 +0,0 @@
-// Export all materi screens for easy importing
-export { default as MateriListScreen } from './MateriListScreen';
-export { default as MateriFormScreen } from './MateriFormScreen';
-export { default as MateriDetailScreen } from './MateriDetailScreen';
-export { default as MateriStatsScreen } from './MateriStatsScreen';
