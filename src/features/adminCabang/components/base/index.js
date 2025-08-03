d666f50d747925de7c47543ce1db78644b2dde67
aditya2708 <adityahadywijaya@gmail.com>
2025-08-03 12:23:59 +0700

    Delete src/features/adminCabang directory

--- src/features/adminCabang/components/base/index.js
+++
@@ -1,3 +0,0 @@
-export { default as BaseFormScreen } from './BaseFormScreen';
-export { default as BaseCRUDScreen } from './BaseCRUDScreen';
-export { default as BaseFilterModal } from './BaseFilterModal';
--- src/features/adminCabang/components/specific/mataPelajaran/index.js
+++
@@ -1,4 +0,0 @@
-export { default as MataPelajaranCard } from './MataPelajaranCard';
-export { default as MataPelajaranForm } from './MataPelajaranForm';
-export { default as MataPelajaranFilter } from './MataPelajaranFilter';
-export { default as KategoriSelector } from './KategoriSelector';
