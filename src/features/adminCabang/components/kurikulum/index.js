b89d2d06c798e2bab5a0c5322f75dc0b43de8dbe
aditya2708 <adityahadywijaya@gmail.com>
2025-08-09 16:20:43 +0700

    Delete src/features/adminCabang directory

--- src/features/adminCabang/components/kurikulum/index.js
+++
@@ -1,4 +0,0 @@
-export { default as BreadcrumbNavigation } from './BreadcrumbNavigation';
-export { default as MateriCard } from './MateriCard';
-export { default as FileUploadInput } from './FileUploadInput';
-export { default as DragDropList, SimpleDragDropList } from './DragDropList';
